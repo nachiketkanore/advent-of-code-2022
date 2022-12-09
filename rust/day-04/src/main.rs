8dfd38e0275f27cb152c3600547ef266e2ae10b5
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-12-09 20:01:38 +0530

    idiomatic rust (trying)

--- rust/day-04/src/main.rs
+++ rust/day-04/src/main.rs
@@ -7,22 +7,23 @@ fn solve_part1(input: &str) -> u32 {
         .into_iter()
         .map(|line| {
             line.split(',').fold(Vec::<u32>::new(), |mut result, half| {
-                half.split('-').for_each(|val| {
-                    result.push(val.parse().unwrap());
-                });
+                result.append(
+                    &mut half
+                        .split('-')
+                        .map(|val| val.parse().unwrap())
+                        .collect::<Vec<u32>>(),
+                );
                 result
             })
         })
-        .fold(0, |ans, vals: Vec<u32>| {
+        .fold(0, |mut ans, vals: Vec<u32>| {
             let (l1, r1, l2, r2) = (vals[0], vals[1], vals[2], vals[3]);
-            dbg!(l1, r1, l2, r2);
-            let mut add = 0;
             if l1 <= l2 && r2 <= r1 {
-                add += 1;
+                ans += 1;
             } else if l2 <= l1 && r1 <= r2 {
-                add += 1;
+                ans += 1;
             }
-            ans + add
+            ans
         })
 }
 
@@ -32,25 +33,27 @@ fn solve_part2(input: &str) -> u32 {
         .into_iter()
         .map(|line| {
             line.split(',').fold(Vec::<u32>::new(), |mut result, half| {
-                half.split('-').for_each(|val| {
-                    result.push(val.parse().unwrap());
-                });
+                result.append(
+                    &mut half
+                        .split('-')
+                        .map(|val| val.parse().unwrap())
+                        .collect::<Vec<u32>>(),
+                );
                 result
             })
         })
-        .fold(0, |ans, vals: Vec<u32>| {
+        .fold(0, |mut ans, vals: Vec<u32>| {
             let (l1, r1, l2, r2) = (vals[0], vals[1], vals[2], vals[3]);
-            let mut add = 0;
-            if (l1 <= l2 && l2 <= r1) {
-                add += 1;
-            } else if (l1 <= r2 && r2 <= r1) {
-                add += 1;
-            } else if (l2 <= l1 && l1 <= r2) {
-                add += 1;
-            } else if (l2 <= r1 && r1 <= r2) {
-                add += 1;
+            if l1 <= l2 && l2 <= r1 {
+                ans += 1;
+            } else if l1 <= r2 && r2 <= r1 {
+                ans += 1;
+            } else if l2 <= l1 && l1 <= r2 {
+                ans += 1;
+            } else if l2 <= r1 && r1 <= r2 {
+                ans += 1;
             }
-            ans + add
+            ans
         })
 }
 
