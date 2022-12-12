6e62ecee6392e18a1c571368bde6010eeb9717a1
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-12-12 21:08:40 +0530

    solved day 07 in rust

--- rust/day-07/src/main.rs
+++ rust/day-07/src/main.rs
@@ -1,111 +1,225 @@
 #![allow(unused)]
 fn main() {}
 
-#[derive(Debug)]
-struct Node {
-    up: Option<Box<Node>>,
-    children: Vec<Option<Box<Node>>>,
-    name: String,
-    val: u32,
-    is_dir: bool,
+#[derive(Debug, Clone, PartialEq)]
+enum NodeType {
+    DIRECTORY,
+    FILE,
 }
 
-fn dfs(curr: Box<Node>, mut final_ans: &u32) -> u32 {
-    let mut ans: u32 = curr.val;
-    for child in curr.children {
-        if let v = child.unwrap() {
-            ans += dfs(v, final_ans);
+#[derive(Debug, Clone)]
+struct Node {
+    name: String,
+    value: u32,
+    node_type: NodeType,
+    id: usize,
+}
+
+#[derive(Debug, Clone)]
+struct GNode {
+    name: String,
+    value: u32,
+    node_type: NodeType,
+    id: usize,
+}
+
+fn dfs_part1(node: GNode, graph: &Vec<Vec<GNode>>, values: &Vec<u32>, answer: &mut u32) -> u32 {
+    let mut total = values[node.id];
+    for child in &graph[node.id] {
+        total += dfs_part1(child.clone(), graph, values, answer);
+    }
+    if node.node_type == NodeType::DIRECTORY {
+        if total <= 100_000 {
+            *answer += total;
         }
     }
-    dbg!(&curr.name, ans);
-    // if curr.is_dir && ans <= 100000 {
-    //     *final_ans += ans;
-    // }
-    ans
+    total
+}
+
+fn dfs_part2(
+    node: GNode,
+    graph: &Vec<Vec<GNode>>,
+    values: &Vec<u32>,
+    dir_sizes: &mut Vec<u32>,
+) -> u32 {
+    let mut total = values[node.id];
+    for child in &graph[node.id] {
+        total += dfs_part2(child.clone(), graph, values, dir_sizes);
+    }
+    if node.node_type == NodeType::DIRECTORY {
+        dir_sizes.push(total);
+    }
+    total
 }
 
 fn solve_part1(input: &str) -> u32 {
-    let mut head: Box<Node> = Box::new(Node {
-        up: None,
-        children: Vec::new(),
-        name: String::from("/"),
-        val: 0,
-        is_dir: true,
-    });
-
-    let start = head;
+    let mut dir_tree: Vec<Node> = vec![];
+    let mut graph: Vec<Vec<GNode>> = Vec::new();
+    let mut values = vec![0u32; 10000];
+    for _ in 0..10000 {
+        graph.push(Vec::new());
+    }
+    let mut id = 0;
 
     for line in input.lines() {
-        // is a command
-        if line.starts_with('$') {
-            let line: Vec<&str> = line.split(' ').collect();
-            // dbg!(&line);
-            match line[1] {
-                "cd" => {
-                    let dir_name = line[2].to_string();
-                    if dir_name == "/" {
-                    } else if dir_name == ".." {
-                        head = head.up.unwrap();
-                    } else {
-                        let mut found = false;
-                        // dbg!(&head);
-                        // assert!(&head.children.len() > &0);
-                        for child in &head.children {
-                            // very bad implementation, will improve later?
-                            // not sure if this is still going to work or not?
-                            // dbg!(&child);
-                            if *child.unwrap().name == dir_name {
-                                head = child.unwrap();
-                                found = true;
-                                break;
-                            }
+        let tokens: Vec<&str> = line.split(' ').collect();
+        match tokens[..] {
+            ["$", "cd", dir_name] => match dir_name {
+                ".." => {
+                    dir_tree.pop();
+                }
+                "/" => dir_tree.push(Node {
+                    name: "/".to_string(),
+                    value: 0,
+                    node_type: NodeType::DIRECTORY,
+                    id: 0,
+                }),
+                dir_name => {
+                    let parent = dir_tree.last().unwrap().id;
+                    let mut nid = 0;
+                    for child in &graph[parent] {
+                        if child.name == dir_name {
+                            nid = child.id;
+                            break;
                         }
-                        assert!(found);
                     }
+
+                    dir_tree.push(Node {
+                        name: dir_name.to_string(),
+                        value: 0,
+                        node_type: NodeType::DIRECTORY,
+                        id: nid,
+                    })
                 }
-                "ls" => {
-                    // do nothing?
-                }
-                _ => panic!(),
+            },
+            ["$", "ls"] => {}
+            ["dir", dir_name] => {
+                let parent = dir_tree.last().unwrap().id;
+                id += 1;
+                graph[parent].push(GNode {
+                    name: dir_name.to_string(),
+                    value: 0,
+                    node_type: NodeType::DIRECTORY,
+                    id,
+                });
             }
-        } else {
-            let line: Vec<&str> = line.split(' ').collect();
-            match line[0] {
-                "dir" => {
-                    let dir_name = line[1];
-                    let add = Node {
-                        up: Some(head),
-                        children: Vec::new(),
-                        name: String::from(dir_name),
-                        val: 0,
-                        is_dir: true,
-                    };
-                    head.children.push(Some(Box::new(add)));
-                    dbg!("pushed into ", &head);
-                }
-                _ => {
-                    let file_size: u32 = line[0].parse().unwrap();
-                    let file_name = line[1];
-                    let add = Node {
-                        up: Some(head),
-                        children: Vec::new(),
-                        name: String::from(file_name),
-                        val: file_size,
-                        is_dir: false,
-                    };
-                    head.children.push(Some(Box::new(add)));
-                    dbg!("pushed into ", &head);
-                }
+            [file_size, file_name] => {
+                let parent = dir_tree.last().unwrap().id;
+                id += 1;
+                graph[parent].push(GNode {
+                    name: file_name.to_string(),
+                    value: file_size.parse().unwrap(),
+                    node_type: NodeType::FILE,
+                    id,
+                });
+                values[id] = file_size.parse().unwrap();
             }
+            _ => panic!(),
         }
     }
-    let mut final_ans = 0;
-    dfs(start, &final_ans);
-    final_ans
+
+    let mut answer = 0;
+    dfs_part1(
+        GNode {
+            name: "/".to_string(),
+            value: 0,
+            node_type: NodeType::DIRECTORY,
+            id: 0,
+        },
+        &graph,
+        &values,
+        &mut answer,
+    );
+    answer
 }
 
 fn solve_part2(input: &str) -> u32 {
-    0
+    let mut dir_tree: Vec<Node> = vec![];
+    let mut graph: Vec<Vec<GNode>> = Vec::new();
+    let mut values = vec![0u32; 10000];
+    for _ in 0..10000 {
+        graph.push(Vec::new());
+    }
+    let mut id = 0;
+
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split(' ').collect();
+        match tokens[..] {
+            ["$", "cd", dir_name] => match dir_name {
+                ".." => {
+                    dir_tree.pop();
+                }
+                "/" => dir_tree.push(Node {
+                    name: "/".to_string(),
+                    value: 0,
+                    node_type: NodeType::DIRECTORY,
+                    id: 0,
+                }),
+                dir_name => {
+                    let parent = dir_tree.last().unwrap().id;
+                    let mut nid = 0;
+                    for child in &graph[parent] {
+                        if child.name == dir_name {
+                            nid = child.id;
+                            break;
+                        }
+                    }
+
+                    dir_tree.push(Node {
+                        name: dir_name.to_string(),
+                        value: 0,
+                        node_type: NodeType::DIRECTORY,
+                        id: nid,
+                    })
+                }
+            },
+            ["$", "ls"] => {}
+            ["dir", dir_name] => {
+                let parent = dir_tree.last().unwrap().id;
+                id += 1;
+                graph[parent].push(GNode {
+                    name: dir_name.to_string(),
+                    value: 0,
+                    node_type: NodeType::DIRECTORY,
+                    id,
+                });
+            }
+            [file_size, file_name] => {
+                let parent = dir_tree.last().unwrap().id;
+                id += 1;
+                graph[parent].push(GNode {
+                    name: file_name.to_string(),
+                    value: file_size.parse().unwrap(),
+                    node_type: NodeType::FILE,
+                    id,
+                });
+                values[id] = file_size.parse().unwrap();
+            }
+            _ => panic!(),
+        }
+    }
+
+    let mut dir_sizes: Vec<u32> = Vec::new();
+    dfs_part2(
+        GNode {
+            name: "/".to_string(),
+            value: 0,
+            node_type: NodeType::DIRECTORY,
+            id: 0,
+        },
+        &graph,
+        &values,
+        &mut dir_sizes,
+    );
+    // size of home directory
+    let all: u32 = *dir_sizes.iter().max().unwrap();
+    let req = 300_00_000 - (700_00_000 - all);
+
+    dir_sizes
+        .into_iter()
+        .filter(|size| size >= &req)
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -118,19 +232,19 @@ mod tests {
         assert_eq!(solve_part1(input), 95437);
     }
 
-    // #[test]
+    #[test]
     fn samples_part2() {
         let input = include_str!("../data/sample.txt");
         assert_eq!(solve_part2(input), 24933642);
     }
 
-    // #[test]
+    #[test]
     fn part1() {
         let input = include_str!("../data/input.txt");
         assert_eq!(solve_part1(input), 1428881);
     }
 
-    // #[test]
+    #[test]
     fn part2() {
         let input = include_str!("../data/input.txt");
         assert_eq!(solve_part2(input), 10475598);
