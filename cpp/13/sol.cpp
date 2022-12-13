f56fd11598d9e74429c14483ec43b1d8bc006131
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-12-13 20:41:55 +0530

    failing c++ solution

--- cpp/13/sol.cpp
+++ cpp/13/sol.cpp
@@ -1,8 +1,9 @@
 /**
  *    Author: Nachiket Kanore
  *    Created: Tuesday 13 December 2022 10:29:52 AM IST
-**/
+ **/
 #include "bits/stdc++.h"
+#include <cctype>
 using namespace std;
 
 #ifdef DEBUG
@@ -17,19 +18,164 @@ using namespace std;
 #define F0R(i, R) for (int i = (0); i < (R); ++i)
 #define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
 
-void solve() {
+bool is_integer(int i, int j, const string& S) {
+	bool ok = true;
+	FOR(id, i, j) ok &= isdigit(S[id]);
+	return ok;
+}
+
+bool check_empty(int i, int j, const string& S) {
+	assert(S[i] == '[' && S[j] == ']');
+	return sz(S) == 2;
+}
+
+bool is_list(int i, int j, const string& S) {
+	return S[i] == '[' && S[j] == ']';
+}
+
+int get_first_value(int i, int j, const string& S) {
+	const int N = S.size();
+	FOR(id, i, j) {
+		if (isdigit(S[id]) && id + 1 <= j && isdigit(S[id + 1])) {
+			return (S[id] - '0') * 10 + (S[id + 1] - '0');
+		}
+		if (isdigit(S[id])) {
+			return S[id] - '0';
+		}
+	}
+	string temp = S.substr(i, j - i + 1);
+	see("**************");
+	see(temp);
+	see("**************");
+	// assert(check_empty(i, j, S));
+	return -1;
+}
+
+vector<pair<int, int>> get_element_indices(int i, int j, const string& S) {
+	vector<pair<int, int>> ret;
+	int si = -1, ei = -1;
+	int bal = 0;
+	for (int id = i + 1; id < j; id++) {
+		if (S[id] == '[') {
+			if (si == -1)
+				si = id;
+			bal += 1;
+		} else if (S[id] == ']') {
+			ei = id;
+			bal -= 1;
+		}
+		if (bal == 0 && ~si && ~ei) {
+			ret.push_back({ si, ei });
+			si = ei = -1;
+		}
+		// see(bal, S[id]);
+		if (bal == 0 && isdigit(S[id])) {
+			// handle '10' in S, there can be 10 in list
+			if (S[id] == '1' && id + 1 < j && S[id + 1] == '0') {
+				ret.push_back({ id, id + 1 });
+				id++;
+			} else {
+				ret.push_back({ id, id });
+			}
+		}
+	}
+	return ret;
+}
+
+int solve(int si, int sj, int ti, int tj, const string& S, const string& T) {
+	if (si > sj || ti > tj)
+		return false;
+	const int N = S.size();
+	const int M = T.size();
+	vector<pair<int, int>> indS = get_element_indices(si, sj, S);
+	vector<pair<int, int>> indT = get_element_indices(ti, tj, T);
+	// see(S, indS);
+	// see(T, indT);
+	// return 0;
+	bool greater_found = false;
+	F0R(i, sz(indS)) {
+		if (i >= sz(indT)) {
+			greater_found = true;
+			break;
+		}
+		auto [xi, xj] = indS[i];
+		auto [yi, yj] = indT[i];
+
+		if (is_integer(xi, xj, S) && is_integer(yi, yj, T)) {
+			int A = stoll(S.substr(xi, xj - xi + 1));
+			int B = stoll(T.substr(yi, yj - yi + 1));
+			// see(S, T);
+			// see("HERE", S.substr(xi, xj - xi + 1), T.substr(yi, yj - yi +
+			// 1)); see("checking", A, B);
+			greater_found |= A > B;
+		} else if (is_list(xi, xj, S) && is_list(yi, yj, T)) {
+			// see("HERE", S.substr(xi, xj - xi + 1), T.substr(yi, yj - yi + 1));
+			bool eS = check_empty(xi, xj, S);
+			bool eT = check_empty(yi, yj, T);
+			if (eS && eT)
+				continue;
+			if (eS && !eT)
+				continue;
+			if (!eS && eT) {
+				greater_found = true;
+				continue;
+			}
+
+			greater_found |= solve(xi, xj, yi, yj, S, T);
+		} else {
+			// need to be compared with first value in other
+			if (is_integer(xi, xj, S)) {
+				int A = stoll(S.substr(xi, xj - xi + 1));
+				int B = get_first_value(yi, yj, T);
+				if (~B)
+					greater_found |= A > B;
+				else {
+					greater_found = true;
+				}
+				// see("comparing", A, B);
+			} else if (is_integer(yi, yj, T)) {
+				int A = get_first_value(xi, xj, S);
+				int B = stoll(T.substr(yi, yj - yi + 1));
+				if (~A)
+					greater_found |= A > B;
+				else {
+					// nothing?
+				}
+				// see("comparing", A, B);
+			}
+		}
+	}
+	return greater_found;
 }
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int T;
-	cin >> T;
+	// auto x = get_element_indices(0, 5, "[[[]]]");
+	// see(x);
+	// return 0;
 
-	while (T--) {
-		solve();
+	string S, T;
+	int ans = 0, idx = 1;
+	while (cin >> S >> T) {
+		bool ok = !solve(0, sz(S) - 1, 0, sz(T) - 1, S, T);
+		if (ok) {
+			// see(S, T);
+			ans += idx;
+		}
+		idx += 1;
 	}
+	cout << ans << '\n';
+	// Observations:
+	// every [ denotes starting of some list
 
+	// [[1],[2,3,4]], [[1],4]
+	// [1], [1]
+	// [2,3,4], 4
+	// [2,3,4], [4]
+	// 2, 4 <- good
+	// 3, null
+	// 4, null
 	return 0;
 }
